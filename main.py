38e4d119bb7618db1da9619f8f12f59fb26a136b
gauravkhatod <gauravkhatod835@gmail.com>
2026-05-20 12:17:05 +0530

    env setup .

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+
+def main():
+    print("Hello from langchain-course!")
+    print("Your OpenAI API key is:", os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
