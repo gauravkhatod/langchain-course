4d58d5f6e66d7d55c52d474a6245e5c73965eabd
gauravkhatod <gauravkhatod835@gmail.com>
2026-05-21 18:44:13 +0530

    Adding langchain env setup

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from dotenv import load_dotenv
-import os 
+import os
+
+from langchain_openai import ChatOpenAI 
 load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
     print("Your OpenAI API key is:", os.environ.get("OPENAI_API_KEY"))
+
+    llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo")
+    response = llm.invoke("What is the capital of France?")
+    print(response.content)
 
 
 if __name__ == "__main__":
